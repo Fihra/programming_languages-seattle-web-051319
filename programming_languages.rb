b09b20f480c7d0e8238d0977f041a551e9ecf0ce
Fabian Fabro <fabrofabian@gmail.com>
2019-05-05 18:34:28 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -11,9 +11,9 @@ require 'pry'
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  languages.each do |style, lang|
+  languages.each do |style, lang_info|
     #binding.pry
-    lang.each do |l, t|
+    lang.each do |lang, type|
       binding.pry
       t.each do |key, value|
         new_hash[l] = {
