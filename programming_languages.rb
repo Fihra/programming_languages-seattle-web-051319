d8e4b8b2e8a43ab7bcf33106945888d83f8a3423
Fabian Fabro <fabrofabian@gmail.com>
2019-05-05 18:33:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,20 @@
 require 'pry'
 
+=begin
+  :language => {
+  :type => interpreted/compiled
+  :style => [:oo, :functional]
+} 
+
+=end
+
 def reformat_languages(languages)
   # your code here
   new_hash = {}
   languages.each do |style, lang|
-    binding.pry
+    #binding.pry
     lang.each do |l, t|
+      binding.pry
       t.each do |key, value|
         new_hash[l] = {
           t: nil,
