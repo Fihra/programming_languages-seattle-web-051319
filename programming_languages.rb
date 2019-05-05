2d2a52427a019a149d3a18020f6fc380e3a3081e
Fabian Fabro <fabrofabian@gmail.com>
2019-05-05 18:28:32 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, lang|
+    lang.each do |l, t|
+      t.each do |key, value|
+        new_hash[l] = {
+          t: nil,
+          style: []
+
+        }
+        binding.pry
+      end
+    end
+  end
+  languages.each do |style|
+  end
 end
+  
